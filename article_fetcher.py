ea6da58e835f05e715f49dfb5c1f3a9946bbb900
Miguel <miguel.garcia.th@gmail.com>
2018-03-07 23:14:28 +0000

    Already parsing JSON from arquivo.pt. We collect date (already in datetime format) and the archive link.

--- article_fetcher.py
+++ article_fetcher.py
@@ -1,5 +1,20 @@
 import requests
+import json
+from datetime import datetime
 
-url = 'http://arquivo.pt/textsearch?q=Albert%20Einstein&maxItems=5&prettyPrint=true'
-response = requests.post(url)
-print response.text
+
+
+def get_articles(name, limit):
+    url = 'http://arquivo.pt/textsearch?prettyPrint=true&versionHistory=%s&maxItems=%s' % (name,limit)
+    response = requests.post(url)
+    return json.loads(response.text)
+
+
+jsonToPython = get_articles('publico.pt', '1')
+intTime = jsonToPython['response_items'][0]['tstamp']
+date = datetime.strptime(intTime, '%Y%m%d%H%M%S').strftime('%d/%m/%Y %H:%M:%S')
+
+
+print(date + ' ' + jsonToPython['response_items'][0]['linkToArchive'])
+
+
