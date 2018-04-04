82187a2e4ebf59b0fa907e3bad12840d5ac409d6
Miguel <miguel.garcia.th@gmail.com>
2018-04-04 22:55:00 +0100

    Esquerda.net is done. Started and finished PSD. We have two repositories of text that is "already classified" as Left wing and right wing. Next step collect publico's data and start to feed some ML algorithm.

--- esquerda_net.py
+++ esquerda_net.py
@@ -5,14 +5,15 @@ from bs4 import BeautifulSoup
 
 url="https://www.esquerda.net/artigos/6?page="
 main_url="https://www.esquerda.net/"
+index=0
 for i in range(0,1):
     page = BeautifulSoup(requests.get(url+str(i)).text, "html.parser")
     productDivs = page.findAll('div', attrs={'class': 'ver-mais-lista'})
     for div in productDivs:
-        print(main_url+div.find('a')['href'])
         article = BeautifulSoup(requests.get(main_url+div.find('a')['href']).text, "html.parser")
         text = article.find('div', attrs={'class': 'field field-name-body'}).findAll('p')
         article_text = ''
+        index+=1
         for element in text:
-            article_text += '\n' + ''.join(element.findAll(text=True))
-        print(article_text)
+            article_text += ''.join(element.findAll(text=True))
+        print(str(index)+", '"+ article_text.replace('"','')+ "', "+ "E, " +main_url+div.find('a')['href']+'\n')
