0a71d5cdf5c2e57b0ded789cc8febed96f128d47
Miguel <miguel.garcia.th@gmail.com>
2018-04-22 16:28:05 +0100

    All collected data is working on weka. The j48 works well with fold training but it still fails on test data.

--- collect_train_data.py
+++ collect_train_data.py
@@ -1,12 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 from WekaFile import WekaFile
-import lxml.html
-import lxml.html.clean
+import re
 
 weka = WekaFile("train_data", "train")
 weka.write_template()
-
+global_index=0
 #ESQUERDA_NET
 url="https://www.esquerda.net/artigos/6?page="
 main_url="https://www.esquerda.net/"
@@ -19,11 +18,12 @@ for i in range(0,100):#100
         text = article.find('div', attrs={'class': 'field field-name-body'}).findAll('p')
         article_text = ''
         index+=1
+        global_index+=1
         for element in text:
             article_text += ''.join(element.findAll(text=True))
         if len(article_text) > 0:
-            weka.write(index, article_text, "E")
-#cleaner = lxml.html.clean.Cleaner(style=True)
+            weka.write(global_index, article_text, "E")
+
 #PSD
 url="http://www.psd.pt/noticias/"
 index=0 #697
@@ -35,12 +35,23 @@ for i in range(1,697):
         text = article.find('div', attrs={'class': 'item_interior_texto_grande'}).findAll('p', attrs={'style': 'text-align:justify'})
         article_text = ''
         index += 1
+        global_index += 1
         for element in text:
-            #doc = lxml.html.fromstring(article_text)
-            #doc = cleaner.clean_html(doc)
-            #article_text+=doc.text_content()
             article_text += ''.join(element.findAll(text=True))
         if len(article_text) > 0:
-            weka.write(index, article_text, "D")
+            weka.write(global_index, article_text, "D")
+
+#PS
+url="http://www.ps.pt/category/imprensa/opiniao/page/"
+index=0 #15
+for i in range(1,15):
+    page = BeautifulSoup(requests.get(url+str(i)).text, "html.parser")
+    productDivs = page.findAll('h4', attrs={'class': 'title has-image'})
+    for div in productDivs:
+        article = BeautifulSoup(requests.get(div.find('a')['href']).text, "html.parser")
+        text = article.find('div', attrs={'class': 'entry-content'})
+        text = re.sub(r'\([^)]*\)', '', text.get_text())
+        global_index += 1
+        weka.write(global_index, text, "E")
 
 weka.close()
--- test.py
+++ test.py
@@ -1,12 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
-url="http://www.psd.pt/noticia/"
+from WekaFile import WekaFile
+import re
+
+weka = WekaFile("train_data", "train")
+weka.write_template()
 
 
-article = BeautifulSoup(requests.get(url+str(2910)).text, "html.parser")
-text = article.find('div', attrs={'class': 'item_interior_texto_grande'}).findAll('p')
-print(text)
-article_text = ''
-for element in text:
-    article_text += ''.join(element.findAll(text=True))
-print(article_text.replace('\t','').replace('\n','').replace('  ', ''))
+url="http://www.psd.pt/noticias/"
+index=0 #697
+for i in range(1,697):
+    page = BeautifulSoup(requests.get(url+str(i)).text, "html.parser")
+    productDivs = page.findAll('div', attrs={'class': 'titulo_noticia'})
+    for div in productDivs:
+        article = BeautifulSoup(requests.get(div.find('a')['href']).text, "html.parser")
+        text = article.find('div', attrs={'class': 'item_interior_texto_grande'}).findAll('p', attrs={'style': 'text-align:justify'})
+        article_text = ''
+        index += 1
+        for element in text:
+            article_text += ''.join(element.findAll(text=True))
+        if len(article_text) > 0:
+            weka.write(index, article_text, "D")
+
+weka.close()
